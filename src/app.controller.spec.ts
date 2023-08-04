cd019882ba0935eb8eba77680e65cf30c8697dea
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-08-04 18:17:33 -0300

    refactor: test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to MG Back-end!"', () => {
+      expect(appController.welcome()).toBe('Welcome to MG Back-end');
     });
   });
 });
